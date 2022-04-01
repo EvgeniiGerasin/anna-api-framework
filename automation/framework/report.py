85c75f4830d98dbae12391c4255cff26c0c4e291
e.gerasin <e.gerasin@bars.group>
2022-04-01 12:33:20 +0300

    Добавлены строгость, ссылки, теги, шаг

--- automation/framework/report.py
+++ automation/framework/report.py
@@ -1,14 +1,79 @@
 import json
+from this import s
 import allure
 
 from requests import Response
 
 
 class Report:
+    
+    # severity
+    @staticmethod
+    def severity(level: str):
+        """Putting the severity in report
+
+        Args:
+            level (str): May be one of
+            BLOCKER, CRITICAL, NORMAL, MINOR, TRIVIAL
+        """
+        if level == 'BLOCKER':
+            allure.severity(allure.severity_level.BLOCKER)
+        elif level == 'CRITICAL':
+            allure.severity(allure.severity_level.CRITICAL)
+        elif level == 'NORMAL':
+            return allure.severity(allure.severity_level.NORMAL)
+        elif level == 'MINOR':
+            allure.severity(allure.severity_level.MINOR)
+        elif level == 'TRIVIAL':
+            allure.severity(allure.severity_level.TRIVIAL)
+
+    # links
+    @staticmethod
+    def link(url, name):
+        """Putting the link test in report"""
+        return allure.link(url, name)
 
     @staticmethod
+    def test_case(url, name):
+        """Putting the link of test case in report"""
+        return Report.test_case(url, name)
+
+    @staticmethod
+    def issue(url, name):
+        """Putting the link of issue in report"""
+        return Report.issue(url, name)
+
+    # allure tags
+    @staticmethod
+    def feature(text: str):
+        """Putting the feature test in report"""
+        return allure.feature(text)
+
+    @staticmethod
+    def title(text: str):
+        """Putting the title test in report"""
+        return allure.title(text)
+
+    @staticmethod
+    def epic(text: str):
+        """Putting the epic test in report"""
+        return allure.title(text)
+
+    @staticmethod
+    def story(text: str):
+        """Putting the story test in report"""
+        return allure.title(text)
+
+    # allure step
+    @staticmethod
+    def step(name: str):
+        """Putting the step test in report"""
+        return allure.step(name)
+
+    # other custom attachements
+    @staticmethod
     def put_description(**kwargs):
-        """Putting description in report
+        """Putting a description in report
 
         Args:
             **kwargs: description
@@ -22,7 +87,7 @@ class Report:
 
     @staticmethod
     def put_request(data):
-        """Putting info on request in report
+        """Putting a info on request in report
 
         Args:
             request (any): data
