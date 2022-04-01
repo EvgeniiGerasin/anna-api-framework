542947850f75fae6ea6f26b5e6a4bf96fab0173a
e.gerasin <e.gerasin@bars.group>
2022-04-01 13:14:20 +0300

    обновлен модуль actions

--- automation/framework/actions.py
+++ automation/framework/actions.py
@@ -23,10 +23,10 @@ class Action:
         """
 
         with allure.step(f'GET request'):
-            self._report.put_request(kwargs)
+            self._report.request(kwargs)
             if method == 'GET':
                 responce = self._s.get(self.url, **kwargs)
             elif method == 'POST':
                 responce = self._s.post(self.url, **kwargs)
-            self._report.put_responce(responce)
+            self._report.responce(responce)
         return responce
