02d7eb38cd59a022d787988ecf2b99139325fa2e
e.gerasin <e.gerasin@bars.group>
2022-04-01 13:14:33 +0300

    рефакториг тестового метода

--- automation/tests/test_file.py
+++ automation/tests/test_file.py
@@ -1,4 +1,3 @@
-import allure
 import pytest
 
 from framework import Action
@@ -6,18 +5,19 @@ from framework import Assert
 from framework import Report
 
 
-@allure.epic('Testing API OpenWather')
+@Report.epic('Testing API OpenWather')
+@Report.feature('Testing requests with different methods')
+@Report.link('https://openweathermap.org/', 'OpenWeatherMap')
+@Report.testcase('https://openweathermap.org/current', 'How to make an API call')
 class TestWatherAPI:
 
-    @allure.title('Get current wather by lat and lon')
+    @Report.title('Get current wather by lat and lon and GET method')
+    @Report.severity('CRITICAL')
     @pytest.mark.parametrize(
         'geo',
         [
             ['35', '140'],
             ['35', '141'],
-            ['35', '143'],
-            ['35', '144'],
-            ['35', '148'],
         ]
     )
     def test_call_current_weather_data(self, geo):
@@ -26,7 +26,7 @@ class TestWatherAPI:
         lon = geo[1]
         appid = '43696882c59420349118379491c29716'
         url = 'https://api.openweathermap.org/data/2.5/weather'
-        Report.put_description(
+        Report.description(
             url=url,
             appid=appid,
             lat=lat,
@@ -42,7 +42,43 @@ class TestWatherAPI:
             }
         )
         want = 200
-        with allure.step('Check status code'):
+        with Report.step('Check status code'):
+            Assert.equality(
+                want, got.status_code, "Want {} but got {}".format(want, got)
+            )
+
+    @Report.title('Get current wather by lat and lon and POST method')
+    @Report.severity('NORMAL')
+    @pytest.mark.parametrize(
+        'geo',
+        [
+            ['35', '140'],
+            ['35', '141'],
+        ]
+    )
+    def test_call_current_weather_data_POST(self, geo):
+        method = 'POST'
+        lat = geo[0]
+        lon = geo[1]
+        appid = '43696882c59420349118379491c29716'
+        url = 'https://api.openweathermap.org/data/2.5/weather'
+        Report.description(
+            url=url,
+            appid=appid,
+            lat=lat,
+            lon=lon
+        )
+        session = Action(url)
+        got = session.request(
+            method=method,
+            params={
+                'lat': lat,
+                'lon': lon,
+                'appid': appid,
+            }
+        )
+        want = 200
+        with Report.step('Check status code'):
             Assert.equality(
                 want, got.status_code, "Want {} but got {}".format(want, got)
             )
