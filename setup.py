c906d605fea4f91e8a5c0b065914c5c9b0ad1ed1
e.gerasin <e.gerasin@bars.group>
2022-04-02 23:30:47 +0300

    измененеия в setup

--- setup.py
+++ setup.py
@@ -3,9 +3,16 @@ import setuptools
 
 setuptools.setup(
     name="anna-api-framework",
-    version="0.0.1",
+    version="0.0.3",
     author="Evgenii Gerasin",
     author_email="e.d.gerasin@yandex.ru",
-    description="Framework work for rapid development of API tests and report generation",
-    packages=["anna"]
+    description="Framework for rapid development of API tests and report generation",
+    packages=["anna"],
+    long_description=open("README.md").read(),
+    license="LICENSE.txt",
+    install_requires=[
+        "requests==2.27.*",
+        "pytest==7.1.*",
+        "allure-pytest==2.9.*",
+    ]
 )
