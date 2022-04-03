b242690d50a7d6e4f20766b8d4ad553ce335995e
e.gerasin <e.gerasin@bars.group>
2022-04-03 13:52:31 +0300

    добавлены тест

--- tests/test_anna.py
+++ tests/test_anna.py
@@ -28,3 +28,40 @@ class TestAnna(unittest.TestCase):
             )
         except AssertionError as e:
             assert False, 'want None but {}'.format(e.args)
+
+    def test_asserts_contains(self):
+        Assert.contains(
+            'hello', 'hello',
+            'want "hello" in "hello" got "hello" not in "hello"'
+        )
+
+    def test_asserts_not_contains(self):
+        try:
+            Assert.not_be().contains(
+                "he", "Goodbye", 'want None but have AssertionError'
+            )
+        except AssertionError as e:
+            assert False, 'want None but {}'.format(e.args)
+
+    def test_compare(self):
+        signs = ['=', '==', '!=', '>', '<', '>=', '<=']
+        a = Assert()
+        for sign in signs:
+            if sign == '=':
+                a.compare(1, '=', 1, 'want 1 == 1 got AssertionError')
+            if sign == '==':
+                a.compare(1, '==', 1, 'want 1 == 1 got AssertionError')
+            elif sign == '!=':
+                a.compare(1, '!=', 2, 'want 1 != 2 got AssertionError')
+            elif sign == '>':
+                a.compare(2, '>', 1, 'want 2 > 1 got AssertionError')
+            elif sign == '<':
+                a.compare(1, '<', 2, 'want 1 < 2 got AssertionError')
+            elif sign == '>=':
+                a.compare(1, '>=', 1, 'want 1 >= 1 got AssertionError')
+            elif sign == '>=':
+                a.compare(2, '>=', 1, 'want 2 >= 1 got AssertionError')
+            elif sign == '<=':
+                a.compare(1, '<=', 1, 'want 1 <= 1 got AssertionError')
+            elif sign == '<=':
+                a.compare(1, '<=', 2, 'want 1 <= 1 got AssertionError')
